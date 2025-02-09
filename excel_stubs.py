caa0d612f4411c6b32ce2384e80d0352d85c1612
Philip <github@phaile.uk>
2025-02-09 10:38:53 +0000

    First working excel version

---
+++ excel_stubs.py
@@ -0,0 +1,29 @@
+# imports
+import pandas as pd
+pd.options.mode.chained_assignment = None
+from openpyxl import load_workbook
+import numpy as np
+from scipy.optimize import minimize
+import time
+import itertools
+import pprint
+
+#=======================================================================================================
+def load_dataframes_from_excel():
+    rad_db = xl("RadiatorDatabase[#All]", headers=True)
+    rooms = xl("Rooms[#All]", headers=True)
+    max_sizes = xl("RoomEmittersMaxSizes", headers=True)
+    labour_costs = xl("LabourCosts", headers=True)
+    flow_rate_scenario = xl("FlowRateScenario", headers=True)
+    return rad_db, rooms, max_sizes, labour_costs, flow_rate_scenario
+
+rad_db, rooms, max_sizes, labour_costs, flow_rate_scenario = load_dataframes_from_excel()
+
+home = Home(rooms, max_sizes, rad_db)
+
+flow_temperature = xl("A31")
+
+results = home.minimal_cost_radiators(flow_temperature)
+
+results
+
